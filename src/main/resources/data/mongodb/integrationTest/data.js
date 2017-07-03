7b24faa632ac1fc52179a07d5276c66530baca59
Govindaraju <govindaraju_venugopal@apple.com>
2017-07-03 15:12:41 +0530

    insert data js script issue fixed

--- src/main/resources/data/mongodb/integrationTest/data.js
+++ src/main/resources/data/mongodb/integrationTest/data.js
@@ -7,5 +7,5 @@ db.organization.insert({
     "Name" : "Goutham", "Location" : "NYC", Address : {"Line1" : "27", 'Line2' : "Mission road", "City" :"BNG"}
 });
 //changeset mlysaght:ChangeSet-2
-db.user.insert({ "Name" : "Naveen","Password"});
-db.user.insert({ "Name" : "Goutham"});
+db.user.insert({ "Name" : "Naveen","Password" :""});
+db.user.insert({ "Name" : "Goutham","Password" :""});
